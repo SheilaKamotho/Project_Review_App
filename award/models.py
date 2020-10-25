3518484204c9496b5c8f050e6a3a1513a11ef8c1
Kamotho Sheila <kamothosheila@gmail.com>
2020-10-25 08:37:57 +0300

    add profile model

--- award/models.py
+++ award/models.py
@@ -2,5 +2,27 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'profile/')
+    editor = models.ForeignKey(User,on_delete=models.CASCADE)
+    bio = models.TextField()
+    contact = models.TextField()
+
+    def save_profile(self):
+        self.save()
+
+    @classmethod
+    def save_profile(cls):
+        posts=cls.objects.filter()
+        return posts
+
+    def update_profile(self):
+        self.update()
+
+    def delete_profile():
+        self.delete()
+
+
