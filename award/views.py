6e45e3791c6ec307ab3edd138a014d6027b37c4d
Kamotho Sheila <kamothosheila@gmail.com>
2020-10-25 04:28:06 +0300

    add project.html to display projects

--- award/views.py
+++ award/views.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, render
 from django.http  import HttpResponse
 
 # Create your views here.
 def project(request):
-    return HttpResponse('Best APP')
+    return render(request, 'project.html')
