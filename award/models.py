7bbff59a9a9b0588676e3af344307e72ad9f854f
Kamotho Sheila <kamothosheila@gmail.com>
2020-10-25 13:56:18 +0300

    add search classmethod

--- award/models.py
+++ award/models.py
@@ -37,6 +37,11 @@ class Project(models.Model):
     def save_project(cls):
         project=cls.objects.filter()
         return project
+    
+    @classmethod
+    def search_by_title(cls,search_term):
+        project = cls.objects.filter(title__icontains=search_term)
+        return project
 
     def update_project(self):
         self.update()
